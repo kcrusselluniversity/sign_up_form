d1a899c113bdbc9ccbd17573450bc888ea5a7efa
kcrusselluniversity <kcrussell.university@gmail.com>
2023-03-09 16:31:49 +1100

    Complete JS script

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 // Select DOM required elements
 const pw = document.querySelector("#password");
 const confirm_pw = document.querySelector("#confirm_password");
-const confirm_pw_container = document.querySelector('#confirm_pw_container')
+const confirm_pw_container = document.querySelector('#confirm_pw_container');
+const site_form = document.getElementById('site_form');
+const password_match_container = document.querySelector('#password_match_container');
 
-// Add event listeners for passwords
+// Add event listeners
 const pw_event = pw.addEventListener('input', checkPassword);
 const pw_confirm_event = confirm_pw.addEventListener('input', checkPassword);
-
-// Select password_match_container 
-const password_match_container = document.querySelector('#password_match_container');
 
 // Functions 
 function checkPassword(e){
